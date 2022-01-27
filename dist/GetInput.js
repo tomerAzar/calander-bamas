55f7c277d3d3824b97d658e921cf48c77ba98909
tomerAzar <tomer.azar7@gmail.com>
2022-01-27 19:27:28 +0200

    add clean code in getInput class

--- dist/GetInput.js
+++ dist/GetInput.js
@@ -5,24 +5,24 @@ const prompt = require('prompt-sync')({ sigint: true });
 class GetInput {
     getYesNo(msg) {
         let input = prompt(msg);
-        if (input != "yes" && input != "no")
+        if (input !== "yes" && input !== "no")
             return this.getYesNo(msg);
         else {
-            if (input == "yes")
+            if (input === "yes")
                 return true;
             return false;
         }
     }
     getString(msg) {
-        let input = prompt(`${msg}:  `);
-        if (input.trim().length == 0)
+        let input = prompt(`${msg}  `);
+        if (input.trim().length === 0)
             return this.getString(msg);
         return input;
     }
     getFullDate(msg) {
         console.log(msg);
         const date = this.getValidDate("enter future date dd/mm/yyyy: ");
-        const fullDate = this.getTime("enter time: hh:mm:ss ", date);
+        const fullDate = this.getTime("enter time: hh:mm:ss: ", date);
         return fullDate;
     }
     getTime(msg, date) {
@@ -31,24 +31,17 @@ class GetInput {
         const hours = Number(input.substring(0, 2));
         const minuts = Number(input.substring(3, 5));
         const seconds = Number(input.substring(6, 8));
-        if (!this.isTimeFormat(input, hours, minuts, seconds))
+        if (!this.isTimeFormat(input, hours, minuts, seconds) || !this.isFutureTime(date, today, hours, minuts, seconds))
             return this.getTime(msg, date);
         else {
-            if (!this.isFutureTime(date, today, hours, minuts, seconds))
-                return this.getTime(msg, date);
-            else {
-                date.setHours(hours);
-                date.setMinutes(minuts);
-                date.setSeconds(seconds);
-                return date;
-            }
+            date.setHours(hours);
+            date.setMinutes(minuts);
+            date.setSeconds(seconds);
+            return date;
         }
     }
     isTimeFormat(input, hours, minuts, seconds) {
-        if (input.length != 8 || input[2] != ':' || input[5] != ':' || isNaN(hours) || isNaN(minuts) || isNaN(seconds) || hours < 1 || hours > 23 || minuts < 0 || minuts > 59 || seconds < 0 || seconds > 59)
-            return false;
-        else
-            return true;
+        return (input.length === 8 && input[2] === ':' && input[5] === ':' && !isNaN(hours) && !isNaN(minuts) && !isNaN(seconds) && hours >= 0 && hours <= 23 && minuts >= 0 && minuts <= 59 && seconds >= 0 && seconds <= 59);
     }
     isFutureTime(date, today, hours, minuts, seconds) {
         if (date.toDateString() == today.toDateString()) {
@@ -66,12 +59,10 @@ class GetInput {
         const day = Number(input.substring(0, 2));
         const month = Number(input.substring(3, 5));
         const year = Number(input.substring(6, 10));
-        if (input.length != 10 || input[2] != '/' || input[5] != '/' || isNaN(day) || isNaN(month) || isNaN(year))
+        if (input.length !== 10 || input[2] !== '/' || input[5] !== '/' || isNaN(day) || isNaN(month) || isNaN(year) || !this.isFutureDate(day, month, year))
             return this.getValidDate(msg);
-        else if (this.isFutureDate(day, month, year))
-            return new Date(year, month - 1, day);
         else
-            return this.getValidDate(msg);
+            return new Date(year, month - 1, day);
     }
     isFutureDate(day, month, year) {
         const today = new Date();
@@ -79,9 +70,9 @@ class GetInput {
             return false;
         else {
             const date = new Date(year, month - 1, day);
-            if (year == today.getFullYear() && date.getMonth() < today.getMonth())
+            if (year === today.getFullYear() && date.getMonth() < today.getMonth())
                 return false;
-            if (year == today.getFullYear() && date.getMonth() == today.getMonth() && date.getDay() < today.getDay())
+            if (year === today.getFullYear() && date.getMonth() === today.getMonth() && date.getDay() < today.getDay())
                 return false;
             return true;
         }
--- src/GetInput.ts
+++ src/GetInput.ts
@@ -2,31 +2,31 @@ const prompt = require('prompt-sync')({sigint: true});
 
 export class GetInput 
 {
-    public getYesNo (msg : string): boolean {
+    getYesNo (msg : string): boolean {
         let input : string =  prompt(msg);
-        if (input != "yes" && input != "no")
+        if (input !== "yes" && input !== "no")
             return this.getYesNo(msg);
         else
         {
-            if(input == "yes") return true;
+            if(input === "yes") return true;
             return false;
         }
     }
 
-    public getString (msg : string) : string {
-        let input : string =  prompt(`${msg}:  `);
-        if (input.trim().length == 0) return this.getString(msg);
+    getString (msg : string) : string {
+        let input : string =  prompt(`${msg}  `);
+        if (input.trim().length === 0) return this.getString(msg);
         return input;
     }
 
-    public getFullDate (msg : string) : Date{
+    getFullDate (msg : string) : Date{
         console.log(msg);
         const date : Date = this.getValidDate("enter future date dd/mm/yyyy: ");
-        const fullDate : Date = this.getTime("enter time: hh:mm:ss ", date);
+        const fullDate : Date = this.getTime("enter time: hh:mm:ss: ", date);
         return fullDate;
     }
 
-    public getTime (msg : string, date : Date) : Date
+    private getTime (msg : string, date : Date) : Date
     {
         const today = new Date();
         let input : string =  prompt(`${msg}  `);
@@ -34,28 +34,22 @@ export class GetInput
         const minuts : number = Number(input.substring(3,5));
         const seconds : number = Number(input.substring(6,8));
 
-        if (!this.isTimeFormat(input, hours,minuts,seconds))
+        if (!this.isTimeFormat(input, hours,minuts,seconds) || !this.isFutureTime(date, today, hours, minuts,seconds) )
             return this.getTime(msg,date);
         else
         {
-            if(!this.isFutureTime(date, today, hours, minuts,seconds))
-                return this.getTime(msg,date);
-            else
-            {
-                date.setHours(hours);
-                date.setMinutes(minuts);
-                date.setSeconds(seconds);
-                return date;
-            }   
-        }
+            date.setHours(hours);
+            date.setMinutes(minuts);
+            date.setSeconds(seconds);
+            return date;
+        }        
     }
-    public isTimeFormat (input: string, hours : number, minuts : number, seconds:number) : boolean{
-        if (input.length !=8 || input[2] != ':' || input[5]!=':' || isNaN(hours) || isNaN(minuts)|| isNaN(seconds) || hours<1 || hours > 23 || minuts<0 || minuts>59 || seconds<0 || seconds>59)
-        return false;
-        else
-        return true;
+
+    private isTimeFormat (input: string, hours : number, minuts : number, seconds:number) : boolean{
+        return  (input.length ===8 && input[2] === ':' && input[5]=== ':' && !isNaN(hours) && !isNaN(minuts) && !isNaN(seconds) && hours>=0 && hours <= 23 && minuts>=0 && minuts<=59 && seconds>=0 && seconds<=59)
     }
-    public isFutureTime (date : Date, today : Date, hours:number , minuts:number,seconds:number) : boolean
+
+    private isFutureTime (date : Date, today : Date, hours:number , minuts:number,seconds:number) : boolean
     {
         if(date.toDateString() == today.toDateString())
         {
@@ -66,30 +60,28 @@ export class GetInput
         return true;
     }
     
-    public getValidDate (msg : string) : Date
+    private getValidDate (msg : string) : Date
     {
         let input : string =  prompt(`${msg}  `);
         const day : number = Number(input.substring(0,2));
         const month : number = Number(input.substring(3,5));
         const year : number = Number(input.substring(6,10));
 
-        if(input.length !=10 || input[2] !='/' || input[5] !='/'|| isNaN(day) || isNaN(month) || isNaN(year))
+        if(input.length !==10 || input[2] !=='/' || input[5] !=='/'|| isNaN(day) || isNaN(month) || isNaN(year) || !this.isFutureDate(day,month,year))
             return this.getValidDate(msg);
-        else if (this.isFutureDate(day,month,year))
+        else 
             return new Date(year,month-1,day );
-        else
-            return this.getValidDate(msg);
     }
 
-    public isFutureDate (day:number , month:number, year:number) : boolean
+    private isFutureDate (day:number , month:number, year:number) : boolean
     {
         const today = new Date();
         if (day<1 || day>31 || month<1 || month>12 || year<today.getFullYear()) return false;
         else
         {
             const date : Date = new Date(year,month-1,day);
-            if(year == today.getFullYear() && date.getMonth() < today.getMonth()) return false;
-            if(year == today.getFullYear() && date.getMonth() == today.getMonth() && date.getDay() < today.getDay()) return false;
+            if(year === today.getFullYear() && date.getMonth() < today.getMonth()) return false;
+            if(year === today.getFullYear() && date.getMonth() === today.getMonth() && date.getDay() < today.getDay()) return false;
             return true;
         }
     }
